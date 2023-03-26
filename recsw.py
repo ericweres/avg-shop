b7146cefa28d4bb0cb1d65ece449534e093dfd68
ericweres <128173839+ericweres@users.noreply.github.com>
2023-03-26 13:12:20 +0200

    init

---
+++ recsw.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import pika, sys, os
+
+def main():
+    connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='shop', exchange_type='direct')
+
+    result = channel.queue_declare(queue='', exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='shop', queue=queue_name, routing_key="sw")
+
+    print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+    def callback(ch, method, properties, body):
+        print(" [x] %r:%r" % (method.routing_key, body.decode()))
+
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
